dc49c765c02aae1de98988941a024b5f917ee248
Rodrigo Gonzalez <rodrigo@axented.com>
2017-06-01 17:32:13 -0500

    Export Weapped Scoket as Socket

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
 export { SocketIoModule }    from './src/socket-io.module';
 export { SocketIoConfig } from './src/config/socket-io.config';
-export { WrappedSocket }   from './src/socket-io.service';
+export { WrappedSocket as Socket }   from './src/socket-io.service';

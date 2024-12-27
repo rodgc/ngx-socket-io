64774f4d2e8e2b9adb0def9797c8fdae44ee9ffb
Gustavo Sverzut Barbieri <barbieri@gmail.com>
2024-12-27 14:22:15 -0300

    Export SOCKET_CONFIG_TOKEN (#188)
    This is needed if one needs to extend a basic configuration with
    namespaces, for example.

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
-export { SocketIoModule }    from './src/socket-io.module';
+export { SocketIoModule, SOCKET_CONFIG_TOKEN } from './src/socket-io.module';
 export { SocketIoConfig } from './src/config/socket-io.config';
 export { WrappedSocket as Socket }   from './src/socket-io.service';
